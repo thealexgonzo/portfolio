3e832bf530c4d4e478633851d496f7418ba00eb8
Alejandro <alexgonzorod@hotmail.com>
2022-03-24 13:14:35 +0000

    added gitignore, made layout responsive, finalised menu

--- script.js
+++ script.js
@@ -1,45 +1,44 @@
-"use strict";
+// "use strict";
 
-const email = document.querySelector(".email");
-const phone = document.querySelector(".phone");
+// const email = document.querySelector(".email");
+// const phone = document.querySelector(".phone");
 
-// const animate = function (name) {
-//   name.addEventListener("click", function () {
-//     document.querySelector(`.${"name"}__animation`).style.animationPlayState =
-//       "running";
-//   });
-// };
+// // const animate = function (name) {
+// //   name.addEventListener("click", function () {
+// //     document.querySelector(`.${"name"}__animation`).style.animationPlayState =
+// //       "running";
+// //   });
+// // };
 
-// animate(email);
+// // animate(email);
 
-// email.addEventListener("click", function () {
-//   document.querySelector(".email__animation").style.animationPlayState =
+// // email.addEventListener("click", function () {
+// //   document.querySelector(".email__animation").style.animationPlayState =
+// //     "running";
+// // });
+
+// phone.addEventListener("click", function () {
+//   document.querySelector(".phone__animation").style.animationPlayState =
 //     "running";
 // });
 
-phone.addEventListener("click", function () {
-  document.querySelector(".phone__animation").style.animationPlayState =
-    "running";
-});
+// email.addEventListener("click", first);
 
-email.addEventListener("click", first);
+// function first(e) {
+//   document.querySelector(".email__animation").style.animationPlayState =
+//     "running";
 
-function first(e) {
-  document.querySelector(".email__animation").style.animationPlayState =
-    "running";
+//   e.stopImmediatePropagation();
+//   this.removeEventListener("click", first);
+//   document.onclick = second;
+// }
 
-  e.stopImmediatePropagation();
-  this.removeEventListener("click", first);
-  document.onclick = second;
-}
+// // Not working as expected, text still appears after second animation run.
+// function second() {
+//   document.querySelector(".email__animation").style.cssText =
+//     "animation: remove-typing 3.5s steps(40, end), remove-caret 0.75s step-end infinite;";
+// }
 
-
-// Not working as expected, text still appears after second animation run. 
-function second() {
-  document.querySelector(".email__animation").style.cssText =
-    "animation: remove-typing 3.5s steps(40, end), remove-caret 0.75s step-end infinite;";
-}
-
-// 1. Make the text appear on first click.
-// 2. Make the text dissapear on second click.
-// 3. How to handle more than 2 clicks 
+// // 1. Make the text appear on first click.
+// // 2. Make the text dissapear on second click.
+// // 3. How to handle more than 2 clicks
